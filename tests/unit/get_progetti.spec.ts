0aaa34ecf2d66abcf23cb0a3a4478078f68822e8
Andrea Rezzi <andrea.rezzi@studenti.unipd.it>
2024-05-14 23:29:50 +0200

    Aggiornato unit test

--- tests/unit/get_progetti.spec.ts
+++ tests/unit/get_progetti.spec.ts
@@ -2,6 +2,8 @@ import { describe, it, expect } from "vitest";
 import { getProgetti } from "../../lambda/get_progetti";
 import { ProgettoDaoMock } from "../../progetto/dao/progetto_dao_mock";
 import { Progetto } from "../../progetto/progetto";
+import { UserMockDao } from "../../user/dao/user_mock_dao";
+import { Role, User } from "../../user/user";
 
 describe("Test get progetti", () => {
   it("returns every project", async () => {
@@ -14,9 +16,16 @@ describe("Test get progetti", () => {
     for (const p of progetti) {
       await progettoDao.insertProgetto(p);
     }
-    expect(await getProgetti(progettoDao)).toStrictEqual({
+
+    const userDao = new UserMockDao();
+    const user = new User("2");
+    user.addToProject(progetti[0].Id, Role.DEV);
+    user.addToProject(progetti[1].Id, Role.PM);
+    await userDao.insertUser(user);
+
+    expect(await getProgetti(progettoDao, userDao, "2")).toStrictEqual({
       statusCode: 200,
-      body: JSON.stringify(progetti),
+      body: JSON.stringify(progetti.slice(0, 2)),
     });
   });
 });
